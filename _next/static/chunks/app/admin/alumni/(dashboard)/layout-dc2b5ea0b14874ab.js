bfebb77b848148295f083f73057776bac511a879
SmartHagar <smarthagar@gmail.com>
2023-12-30 19:37:43 +0900

    tambah dashboard pada admin

---
+++ _next/static/chunks/app/admin/alumni/(dashboard)/layout-dc2b5ea0b14874ab.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160,2861,7151,1456,2084,6806,3789,3577,2733,3481,7546,410,941],{7155:function(){}},function(n){n.O(0,[9253,7698,1744],function(){return n(n.s=7155)}),_N_E=n.O()}]);
